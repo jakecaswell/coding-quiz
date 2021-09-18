a6a7a731b67dbc99ff057a5a067b0b6f77c54542
jcaswell <jakecaswell88@gmail.com>
2021-09-18 01:08:24 -0400

    moved to this repo

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+var time = 50;
+var score = 0;
+var highScore = [];
+
+
+// questions 
+var questions = [
+    {
+        number: "1",
+        question: "",
+        possibleAnswers: [""],
+        correctAnswer: ""
+    },
+    {
+        number: "2",
+        question: "",
+        possibleAnswers: [""],
+        correctAnswer: ""
+    },
+    {
+        number: "3",
+        question: "",
+        possibleAnswers: [""],
+        correctAnswer: ""
+    },
+    {
+        number: "4",
+        question: "",
+        possibleAnswers: [""],
+        correctAnswer: ""
+    },
+    {
+        number: "5",
+        question: "",
+        possibleAnswers: [""],
+        correctAnswer: ""
+    },
+]
